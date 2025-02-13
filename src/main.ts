6deaed31ddf77bb88779d6341f9d1346667234e9
Andrés Pino <pinoandres85@gmail.com>
2025-02-13 17:43:32 -0500

    Fourth commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './style.css'
-import { name } from './types-basic/types-basic'
+import { age, id, name } from './types-basic/types-basic'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>Hello ${name}!</h1>
+  <h1>Hello I'm ${name}, I'm ${age} years old, and my id is ${id}!</h1> 
 `
+// String multilinea con template literals (backticks)s
 
--- src/types-basic/types-basic.ts
+++ src/types-basic/types-basic.ts
@@ -1,2 +1,12 @@
-export const name = 'John Doe';
+export let name:string = 'Andrés Doe';
+export const id:number = 1;
+export const age:number = 20;
 
+export const message:string = `
+Esto es un template string
+puede usar ' ' simples o " " dobles
+tener expresiones ${1+1}
+inyectar valores ${name}
+`;
+
+console.log(message);
